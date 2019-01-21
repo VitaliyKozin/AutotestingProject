5e3d53e73499b1c97b0db57db826e9eb92b2fc33
User <kozinvitalij7@gmail.com>
2019-01-21 17:27:52 +0200

    Commit works

--- KozinProj/src/test/java/loginTests/LoginTest.java
+++ KozinProj/src/test/java/loginTests/LoginTest.java
@@ -8,8 +8,8 @@ public class LoginTest extends ParentTest {
     @Test
     public void validLogIn() {
         loginPage.openPage();
-        loginPage.enterLogin("Student");
-        loginPage.enterPass("909090");
+        loginPage.enterLogin("admin_test");
+        loginPage.enterPass("admin_test");
         loginPage.clickOnSubmitButton();
 
         checkAC("Avatar is not present", homePage.isAvatarPresent(), true);
